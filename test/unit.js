e2cb4a9d315368d48425efc4966051a22bd102f8
varjolintu <sami.vanttinen@protonmail.com>
2018-06-13 13:26:31 +0300

    Add encryption and decryption test

--- test/unit.js
+++ test/unit.js
@@ -32,4 +32,42 @@ describe('Unit tests with background/keepass.js', function() {
         incrementedNonce = nacl.util.decodeBase64(incrementedNonce);
         assert.equal(nonce[0]+1, incrementedNonce[0]);
     });
+
+    it('Test encryption and decryption', function() {
+        var keepass = rewire(BG_URL + 'keepass.js');
+        var nacl = rewire(BG_URL + 'nacl.min.js');
+        nacl.util = rewire(BG_URL + 'nacl-util.min.js');
+
+        var serverPublicKey = nacl.util.decodeBase64('EhpnWP2r3LVllNv3wzASjdEZnJD6ICMkhedegxT2BX8=');
+        var serverSecretKey = nacl.util.decodeBase64('ZhpIDkRYkhz6oujjkBY5HfRJtYAfj7ln/bYqLagUyvg=');
+        var nonce = keepass.getNonce();
+        var clientMessage = '{message: \'a test message\'}';
+        var serverMessage = '{message: \'a test response\', success: \'true\'}';
+
+        keepass.generateNewKeyPair();   // Client keypair
+        keepass.serverPublicKey = serverPublicKey;
+
+        // Client sends this
+        const encryptedClient = keepass.encrypt(clientMessage, nonce);
+        console.log('Encrypted client message: ' + encryptedClient);
+
+        // Simulate a situation where server receives the message and decrypts it
+        const decryptedServer = nacl.box.open(nacl.util.decodeBase64(encryptedClient), nacl.util.decodeBase64(nonce), keepass.keyPair.publicKey, serverSecretKey);
+        var decryptedMessage = JSON.parse(nacl.util.encodeUTF8(decryptedServer));
+        console.log('Decrypted client reply: ' + decryptedMessage);
+        assert.equal(decryptedMessage, clientMessage);
+
+        // Simulates what server does when responding
+        keepass.associated.hash = 'test';
+        keepass.databaseHash = 'test';
+        var incrementedNonce = keepass.incrementedNonce(nonce);
+        const encryptedServer = nacl.box(nacl.util.decodeUTF8(serverMessage), nacl.util.decodeBase64(incrementedNonce), keepass.keyPair.publicKey, serverSecretKey);
+        console.log('Encrypted server message: ' + nacl.util.encodeBase64(encryptedServer));
+
+        // Client decrypts the response
+        const serverReply = keepass.decrypt(nacl.util.encodeBase64(encryptedServer), incrementedNonce);
+        const message = nacl.util.encodeUTF8(serverReply);
+        console.log('Decrypted server reply: ' + message);
+        assert.equal(message, serverMessage);
+    });
 });
